ee274108c2977dd95c299b76a537ebdcbcf3813a
Maiko Blaukopf <maiko-blaukopf@hotmail.com>
2025-07-25 14:49:37 -0400

    tryout3

--- emotion_detection.py
+++ emotion_detection.py
@@ -3,11 +3,11 @@ import json
 
 def emotion_detector(detected_text):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    myobj = { "raw_document": { "text": text_to_analyse } }
+    myobj = { "raw_document": { "text": detected_text } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json=myobj, headers=header)
     formatted_response = json.loads(response.text)
-    if all(value is None for value in text_to_analyse.values()):
+    if all(value is None for value in detected_text.values()):
         return detected_text
     emotions = detected_text['emotionPredictions'][0]['emotion']
     dominant_emotion = max(emotions.items(), key=lambda x: x[1])[0]

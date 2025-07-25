a5fca7d694515401dcaa8a0d896e64264efe3391
Maiko Blaukopf <maiko-blaukopf@hotmail.com>
2025-07-25 15:07:46 -0400

    task 3 finalized

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,29 +1,18 @@
 import requests
 import json
 
-def emotion_detector(detected_text):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    myobj = { "raw_document": { "text": detected_text } }
+def emotion_detector(text_to_analyze):
+    url ='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    response = requests.post(url, json=myobj, headers=header)
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json=input_json, headers=header)
+
+
+    emotions = {}
+
     formatted_response = json.loads(response.text)
-    if all(value is None for value in detected_text.values()):
-        return detected_text
-    emotions = detected_text['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(emotions.items(), key=lambda x: x[1])[0]
-    anger = emotions['anger']
-    disgust = emotions['disgust']
-    fear = emotions['fear']
-    joy = emotions['joy']
-    sadness = emotions['sadness']
-    max_emotion = max(emotions, key=emotions.get)
-    #max_emotion_score = emotions[max_emotion]
-    formatted_dict_emotions = {
-                            'anger': anger,
-                            'disgust': disgust,
-                            'fear': fear,
-                            'joy': joy,
-                            'sadness': sadness,
-                            'dominant_emotion': max_emotion
-                            }
-    return formatted_dict_emotions
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions.items(), key=lambda x: x[1])
+    emotions['dominant_emotion'] = dominant_emotion[0]
+
+    return emotions

59611f74e13a47c803fdee6ff21ae01ba14f492d
Yatindra Rai <yatindrarai9909@gmail.com>
2025-12-28 12:28:07 +0530

    made the database

---
+++ server/djangoapp/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+# Car Make Model
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    country = models.CharField(max_length=50, blank=True, null=True)  # optional extra field
+    founded_year = models.IntegerField(
+        validators=[MinValueValidator(1800), MaxValueValidator(now().year)],
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+# Car Model Model
+class CarModel(models.Model):
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    TRUCK = 'Truck'
+    COUPE = 'Coupe'
+    
+    CAR_TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (TRUCK, 'Truck'),
+        (COUPE, 'Coupe'),
+    ]
+
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name='models')
+    dealer_id = models.IntegerField(null=True, blank=True)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=20, choices=CAR_TYPE_CHOICES)
+    year = models.IntegerField(
+        validators=[MinValueValidator(2015), MaxValueValidator(2023)]
+    )
+    color = models.CharField(max_length=30, blank=True, null=True)  # optional extra field
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year})"
